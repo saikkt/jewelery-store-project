7c75cbdc80c93bd6037cac26647e3b69d7f1347f
saikkt <sai.kkt@gmail.com>
2020-04-11 19:19:37 -0400

    Spring Initializer project

---
+++ jewelry-store/src/test/java/com/eCommerce/jewelrystore/JewelryStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eCommerce.jewelrystore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JewelryStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
