3d05364c8687fd1a1f03b27e2aad9b14193181ec
seethend <seethendreddy96@gmail.com>
2020-05-10 03:35:06 +0530

    seeth - User Details & Password update API added

---
+++ jewelry-store/src/main/java/com/eCommerce/jewelrystore/accounts/exceptions/UserForbiddenException.java
@@ -0,0 +1,9 @@
+package com.eCommerce.jewelrystore.accounts.exceptions;
+
+public class UserForbiddenException extends RuntimeException {
+
+	public UserForbiddenException(String msg) {
+		super(msg);
+	}
+	
+}
