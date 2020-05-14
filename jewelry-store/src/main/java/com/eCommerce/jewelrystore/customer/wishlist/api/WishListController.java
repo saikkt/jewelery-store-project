4e4c34d821c6cac133072061bf7571b5289d0709
akhilsai25 <adevunoori@Virtusa.com>
2020-05-14 15:57:38 -0400

    product-keyword-issue

--- jewelry-store/src/main/java/com/eCommerce/jewelrystore/customer/wishlist/api/WishListController.java
+++ jewelry-store/src/main/java/com/eCommerce/jewelrystore/customer/wishlist/api/WishListController.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -62,4 +63,7 @@ public class WishListController {
         }
         return ResponseEntity.ok().body(wishListModel);
     }
+
+//    @PostMapping("/postWishList")
+//    public WishListModel postWishList()
 }
