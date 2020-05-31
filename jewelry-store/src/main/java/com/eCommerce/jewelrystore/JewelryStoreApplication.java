059a61b055d4f2d2174e76dc2906811aa71b097d
akhilsai25 <adevunoori@Virtusa.com>
2020-05-31 16:27:54 -0400

    config-rollback

--- jewelry-store/src/main/java/com/eCommerce/jewelrystore/JewelryStoreApplication.java
+++ jewelry-store/src/main/java/com/eCommerce/jewelrystore/JewelryStoreApplication.java
@@ -2,29 +2,13 @@ package com.eCommerce.jewelrystore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.services.s3.AmazonS3Client;
-import com.eCommerce.jewelrystore.email.util.MailSenderImpl;
-import org.hibernate.boot.model.naming.ImplicitNamingStrategy;
-import org.hibernate.boot.model.naming.ImplicitNamingStrategyLegacyJpaImpl;
-import org.hibernate.boot.model.naming.PhysicalNamingStrategy;
-import org.hibernate.boot.model.naming.PhysicalNamingStrategyStandardImpl;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
 @EnableScheduling
-@EnableConfigServer
+//@EnableConfigServer
 public class JewelryStoreApplication {
 
 
